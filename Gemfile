0d70c9a80379fe3a8783de8478275193aba859a3
aoki <popdod.ds@yahoo.ne.jp>
2021-05-30 10:36:42 +0900

    add rubocop

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~>3.6.0'
   gem "factory_bot_rails", "~> 4.10.0"
+  gem 'rubocop'
+  gem 'rubocop-rails'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -36,10 +36,10 @@ module James
 
     config.generators do |g|
       g.test_framework :rspec,
-      fixture: true,
-      view_specs: false,
-      helper_specs: false,
-      routing_specs: false
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
     end
   end
 end
