4672bcc25b50ef60dfb0f7dba9e9db383b8e65ba
aoki <popdod.ds@yahoo.ne.jp>
2021-06-20 09:29:20 +0900

    add menudrawer

---
+++ frontend/src/components/molecules/MenuDrawer.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@chakra-ui/button";
+import { Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakra-ui/modal";
+import { memo, VFC } from "react";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  onClickUser: () => void;
+  onClickLogin: () => void;
+};
+
+export const MenuDrawer: VFC<Props> = memo((props) => {
+  const { isOpen, onClose, onClickUser, onClickLogin } = props;
+  return (
+    <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerBody as="nav" p={0} bg="gray.100">
+            <Button w="100%" onClick={onClickUser}>
+              アカウント
+            </Button>
+            <Button w="100%" onClick={onClickLogin}>
+              ログイン
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+  );
+});
--- frontend/src/components/organisms/layout/Header.tsx
+++ frontend/src/components/organisms/layout/Header.tsx
@@ -2,6 +2,8 @@ import { useDisclosure } from "@chakra-ui/hooks";
 import { Box, Flex, Heading, Link } from "@chakra-ui/layout";
 import { memo, useCallback, VFC } from "react";
 import { useHistory } from "react-router";
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -14,32 +16,41 @@ export const Header: VFC = memo(() => {
   const onClickLogin = useCallback(() => history.push("/login"),[]);
   
   return (
-    <Flex
-        as="nav"
-        bg="#76a1b8"
-        color="white"
-        align="center"
-        justify="space-between"
-        padding={{ base:3, md: 5 }}
-      >
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickHome}>
-          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-            JAMES
-          </Heading>
-        </Flex>
-        <Flex
+    <>
+      <Flex
+          as="nav"
+          bg="#76a1b8"
+          color="white"
           align="center"
-          fontSize="sm"
-          display={{ base: "none", md: "flex" }}
-          flexGrow={2}
+          justify="space-between"
+          padding={{ base:3, md: 5 }}
         >
-          <Box pr={4}>
-            <Link onClick={onClickUser}>アカウント</Link>
-          </Box>
-          <Box pr={4}>
-            <Link onClick={onClickLogin}>ログイン</Link>
-          </Box>
+          <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }} onClick={onClickHome}>
+            <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+              JAMES
+            </Heading>
+          </Flex>
+          <Flex
+            align="center"
+            fontSize="sm"
+            display={{ base: "none", md: "flex" }}
+            flexGrow={2}
+          >
+            <Box pr={4}>
+              <Link onClick={onClickUser}>アカウント</Link>
+            </Box>
+            <Box pr={4}>
+              <Link onClick={onClickLogin}>ログイン</Link>
+            </Box>
+          </Flex>
+          <MenuIconButton onOpen={onOpen}/>
         </Flex>
-      </Flex>
+        <MenuDrawer 
+          isOpen={isOpen} 
+          onClose={onClose} 
+          onClickUser={onClickUser} 
+          onClickLogin={onClickLogin}
+        />
+      </>
   );
 });
