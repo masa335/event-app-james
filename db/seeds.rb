7562221ee250b1f42b0bbacc89b0dcb98cfa7ca1
aoki <popdod.ds@yahoo.ne.jp>
2021-06-20 14:37:29 +0900

    add events controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# イベント情報のサンプルデータ
+user = User.first
+
+3.times do |n|
+  event = Event.new(
+    user_id: user.id,
+    event_name: "session#{n}",
+    event_date: Time.zone.now,
+    venue: "studio#{n}"
+  )
+
+  event.save!
+end
