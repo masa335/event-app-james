61a2b9577c39e7520d469de3e6d17ebdafe53cf2
aoki <popdod.ds@yahoo.ne.jp>
2021-06-29 19:21:02 +0900

    fix 不要なパラメータ削除

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
   def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation, :confirm_success_url)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
