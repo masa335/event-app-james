c3ba0f35b50c8cecbc3dea06b0de7d81934361df
aoki <popdod.ds@yahoo.ne.jp>
2021-07-07 06:20:10 +0900

    update ユーザー画面のラフ作成

--- frontend/src/components/pages/User.tsx
+++ frontend/src/components/pages/User.tsx
@@ -1,7 +1,26 @@
 import { memo, VFC } from "react";
+import { Heading, Box, Image, Text } from "@chakra-ui/react"
 
 export const User: VFC = memo(() => {
   return (
-    <p>ユーザー情報表示画面です</p>
+    <>
+    <Box mx="40px" my="20px">
+      <Box borderBottom="6px" borderStyle="double" borderColor="blackAlpha.400">
+        <Image
+            borderRadius="full"
+            boxSize="160px"
+            src="https://source.unsplash.com/random"
+            alt="アオキマサノリ"
+            m="auto"
+          />
+        <Text fontSize="2xl" textAlign="center" color="gray.600">アオキ マサノリ</Text>
+        <Text fontSize="lg" textAlign="center" color="gray.600">ギターとベースができます。よろしくです。</Text>
+      </Box>
+      <Heading as="h2" size="lg" color="gray.600">イベント</Heading>
+      <p>★TODO:主催、参加イベントをタブで分けたい。</p>
+    </Box>
+    
+    
+    </>
   );
 });
