99276281759b6bd5509e4f4101181170451e7ad4
aoki <popdod.ds@yahoo.ne.jp>
2021-07-09 17:13:01 +0900

    update userコントローラにupdateを追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     render json: user, status: :ok
   end
+
+  def update
+    user = User.find(params[:id])
+    if user.update!(user_params)
+      render json: user, status: :ok
+    else
+      render json: user.errors
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :age, :self_introduction)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :events
-      resources :users, only: %i[show]
+      resources :users, only: [:show, :update]
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations'
       }
 
       namespace :auth do
-        resources :sessions, only: %i[index]
+        resources :sessions, only: [:index]
       end
     end
   end
