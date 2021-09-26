0fa456d0422066efcda4b2ed55e8eb467c323396
aoki <popdod.ds@yahoo.ne.jp>
2021-09-26 09:59:17 +0900

    update デフォルトURL変更

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # devise mailer
   config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
