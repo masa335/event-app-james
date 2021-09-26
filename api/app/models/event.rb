4db61ccb1d0b9163ef808204d9148918183a41b6
aoki <popdod.ds@yahoo.ne.jp>
2021-09-26 12:08:05 +0900

    fix バグ修正

--- api/app/models/event.rb
+++ api/app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :user
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   mount_uploader :image, AvaterUploader # CarrierWaveで作ったクラスと紐付ける
 end
