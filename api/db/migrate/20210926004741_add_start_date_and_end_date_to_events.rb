c4579b488119330864609c466319a9e20e31aade
aoki <popdod.ds@yahoo.ne.jp>
2021-09-26 10:00:50 +0900

    add eventsテーブルにカラムを追加

---
+++ api/db/migrate/20210926004741_add_start_date_and_end_date_to_events.rb
@@ -0,0 +1,6 @@
+class AddStartDateAndEndDateToEvents < ActiveRecord::Migration[6.0]
+  def change
+    add_column :events, :start_date, :datetime
+    add_column :events, :end_date, :datetime
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_022504) do
+ActiveRecord::Schema.define(version: 2021_09_26_004741) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_07_21_022504) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
