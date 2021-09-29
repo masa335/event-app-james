f516e2c0b78459d5cd5627f8f34cf317a8e5aae3
aoki <popdod.ds@yahoo.ne.jp>
2021-09-30 08:42:27 +0900

    fix apiエンドポイント修正

--- frontend/src/config/axiosBaseUrl.js
+++ frontend/src/config/axiosBaseUrl.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default axios.create({
-  baseURL: isDevelopment ? 'http://localhost:3000' : 'http://example.com'
+  baseURL: isDevelopment ? 'http://localhost:3000' : 'http://james-alb-1262667731.ap-northeast-1.elb.amazonaws.com/'
 });
