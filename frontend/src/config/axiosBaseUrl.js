c853f8ea974b0240a7cd5a8a70064de94f24f524
aoki <popdod.ds@yahoo.ne.jp>
2021-09-30 12:07:32 +0900

    fix APIエンドポイント変更

--- frontend/src/config/axiosBaseUrl.js
+++ frontend/src/config/axiosBaseUrl.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default axios.create({
-  baseURL: isDevelopment ? 'http://localhost:3000' : 'http://james-alb-1262667731.ap-northeast-1.elb.amazonaws.com/'
+  baseURL: isDevelopment ? 'http://localhost:3000' : 'https://masa335.link/'
 });
