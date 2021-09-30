f72eed793bc58a44cb456a394bae76e555ac9aa6
aoki <popdod.ds@yahoo.ne.jp>
2021-10-01 07:18:50 +0900

    update 設定追加

--- frontend/src/config/axiosBaseUrl.js
+++ frontend/src/config/axiosBaseUrl.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default axios.create({
+  withCredentials: true,
   baseURL: isDevelopment ? 'http://localhost' : 'https://masa335.link'
 });
