81587056a2765c488933f4043a90ba29e01f4a06
aoki <popdod.ds@yahoo.ne.jp>
2021-09-30 14:48:05 +0900

    fix CORS対応

--- frontend/src/config/axiosBaseUrl.js
+++ frontend/src/config/axiosBaseUrl.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default axios.create({
-  baseURL: isDevelopment ? 'http://localhost:3000' : 'https://event-app-james.vercel.app'
+  baseURL: isDevelopment ? 'http://localhost:3000' : 'https://masa335.link'
 });
