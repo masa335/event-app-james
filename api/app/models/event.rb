fb1a8b40a795f3e0cd320442a635e509705d5588
aoki <popdod.ds@yahoo.ne.jp>
2021-10-02 11:44:24 +0900

    update commentsテーブルと関連付け

--- api/app/models/event.rb
+++ api/app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :memberships, dependent: :destroy
+  has_many :comments, dependent: :destroy
   mount_uploader :image, AvaterUploader # CarrierWaveで作ったクラスと紐付ける
 end
