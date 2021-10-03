02245d16064cca4547eb7dcaf1b1565b1036f516
aoki <popdod.ds@yahoo.ne.jp>
2021-10-04 06:36:50 +0900

    update フォローフォロワー一覧のUI変更

--- frontend/src/components/pages/FollowingAndFollower.tsx
+++ frontend/src/components/pages/FollowingAndFollower.tsx
@@ -1,5 +1,5 @@
 import { memo, useEffect, VFC } from "react";
-import { Stack, Link, Tabs, TabPanels, TabPanel, TabList, Tab } from "@chakra-ui/react";
+import { Stack, Link, Tabs, TabPanels, TabPanel, TabList, Tab, Text, Image, HStack } from "@chakra-ui/react";
 import { useHistory, useParams } from "react-router-dom";
 import { useUser } from "../../hooks/useUser";
 
@@ -32,16 +32,42 @@ export const FollowingAndFollower: VFC<Props> = memo(props => {
       </TabList>
       <TabPanels>
         <TabPanel>
-          <Stack spacing={4}>
+          <Stack maxW={500} spacing={4} m="auto">
             {following.map((following) => (
-              <Link key={following.id}>{following.name}</Link>
+              <Link href={`/user/${following.id}`} key={following.id}>
+              <HStack p={2} borderWidth="1px" borderRadius="lg" overflow="hidden" shadow="md" bg="gray.100">
+                <Image
+                  borderRadius="full"
+                  boxSize="50px"
+                  src={following.image.url ?? `${process.env.PUBLIC_URL}/default_icon.png`}
+                  alt={following.name}
+                />
+                <Stack>
+                  <Text fontSize="sm">{following.name}</Text>
+                  <Text >{following.self_introduction}</Text>
+                </Stack>
+              </HStack>
+            </Link>
             ))}
           </Stack>
         </TabPanel>
         <TabPanel>
-          <Stack spacing={4}>
+          <Stack maxW={500} spacing={4} m="auto">
             {followers.map((follower) => (
-              <Link key={follower.id}>{follower.name}</Link>
+              <Link href={`/user/${follower.id}`} key={follower.id}>
+                <HStack p={2} borderWidth="1px" borderRadius="lg" overflow="hidden" shadow="md" bg="gray.100">
+                  <Image
+                    borderRadius="full"
+                    boxSize="50px"
+                    src={follower.image.url ?? `${process.env.PUBLIC_URL}/default_icon.png`}
+                    alt={follower.name}
+                  />
+                  <Stack>
+                    <Text fontSize="sm">{follower.name}</Text>
+                    <Text >{follower.self_introduction}</Text>
+                  </Stack>
+                </HStack>
+              </Link>
             ))}
           </Stack>
         </TabPanel>
