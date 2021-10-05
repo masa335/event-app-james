f87a934fccd2c827a451a2818f7bb3e59d69b158
aoki <popdod.ds@yahoo.ne.jp>
2021-10-05 16:06:21 +0900

    update バリデーションのテスト追加

--- api/spec/factories/events.rb
+++ api/spec/factories/events.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :event do
-    
+    event_name 'test event'
+    event_category '0'
+    prefecture_id '20'
+    venue 'hoge studio'
+    explanation 'hoge'
+    start_date Time.local(2021, 10, 5, 21, 0, 0, 0)
+    end_date Time.local(2021, 10, 5, 23, 30, 0, 0)
+    max_participants 5
+    association :user
   end
 end
