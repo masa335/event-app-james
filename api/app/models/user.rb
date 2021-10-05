387e55232f4e49ef1c64d998bd0991fc24cb44f7
aoki <popdod.ds@yahoo.ne.jp>
2021-10-05 11:28:28 +0900

    update バリデーション追加

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -9,7 +9,13 @@ class User < ActiveRecord::Base
   has_many :followings, through: :active_relationships, source: :follower
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :passive_relationships, source: :followed
+
   mount_uploader :image, AvaterUploader # CarrierWaveで作ったクラスと紐付ける
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :age, length: { maximum: 3 }, numericality: { only_integer: true }
+  validates :self_introduction, length: { maximum: 200 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
